19e76d06a09afca70373afb15d83d8c4a930ec28
kendallm <1841529+kendallm@users.noreply.github.com>
2020-03-13 17:42:48 -0700

    Implement Monad module

--- src/Course/Monad.hs
+++ src/Course/Monad.hs
@@ -37,8 +37,8 @@ instance Monad ExactlyOne where
     -> ExactlyOne a
     -> ExactlyOne b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance ExactlyOne"
-
+    -- f . runExactlyOne
+    bindExactlyOne
 -- | Binds a function on a List.
 --
 -- >>> (\n -> n :. n :. Nil) =<< (1 :. 2 :. 3 :. Nil)
@@ -49,7 +49,7 @@ instance Monad List where
     -> List a
     -> List b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance List"
+    flatMap
 
 -- | Binds a function on an Optional.
 --
@@ -61,7 +61,7 @@ instance Monad Optional where
     -> Optional a
     -> Optional b
   (=<<) =
-    error "todo: Course.Monad (=<<)#instance Optional"
+    bindOptional
 
 -- | Binds a function on the reader ((->) t).
 --
@@ -72,8 +72,8 @@ instance Monad ((->) t) where
     (a -> ((->) t b))
     -> ((->) t a)
     -> ((->) t b)
-  (=<<) =
-    error "todo: Course.Monad (=<<)#instance ((->) t)"
+  (=<<) fa fta t =
+    ((fa . fta) t) t
 
 -- | Witness that all things with (=<<) and (<$>) also have (<*>).
 --
@@ -111,8 +111,8 @@ instance Monad ((->) t) where
   k (a -> b)
   -> k a
   -> k b
-(<**>) =
-  error "todo: Course.Monad#(<**>)"
+(<**>) fab fa =
+  (<$> fa) =<< fab
 
 infixl 4 <**>
 
@@ -133,8 +133,8 @@ join ::
   Monad k =>
   k (k a)
   -> k a
-join =
-  error "todo: Course.Monad#join"
+join fa =
+  id =<< fa
 
 -- | Implement a flipped version of @(=<<)@, however, use only
 -- @join@ and @(<$>)@.
@@ -147,8 +147,8 @@ join =
   k a
   -> (a -> k b)
   -> k b
-(>>=) =
-  error "todo: Course.Monad#(>>=)"
+(>>=) a fab =
+  join (fab <$> a)
 
 infixl 1 >>=
 
@@ -163,8 +163,10 @@ infixl 1 >>=
   -> (a -> k b)
   -> a
   -> k c
-(<=<) =
-  error "todo: Course.Monad#(<=<)"
+(<=<) fbc  =
+  -- fbc =<< (fab a)
+  -- (fbc =<<) . fab
+  (.) (fbc =<<)  
 
 infixr 1 <=<
 
