b8f52d4c46381cfdf736bb6350abee465498be46
kendallm <1841529+kendallm@users.noreply.github.com>
2020-03-14 17:18:51 -0700

    Implement IO Module

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -85,46 +85,55 @@ printFile ::
   FilePath
   -> Chars
   -> IO ()
-printFile =
-  error "todo: Course.FileIO#printFile"
+  
+-- putStrLn ("============ " ++ filePath) >>= (\_ -> putStrLn contents)
+printFile filePath contents = do
+  putStrLn ("============ " ++ filePath)
+  putStrLn contents
 
 -- Given a list of (file name and file contents), print each.
 -- Use @printFile@.
 printFiles ::
   List (FilePath, Chars)
   -> IO ()
-printFiles =
-  error "todo: Course.FileIO#printFiles"
+printFiles files =
+  -- foldRight (\file _ -> printFile (fst file) (snd file)) (pure ())
+  let f  = (\file -> printFile (fst file) (snd file)) 
+  in void (sequence (f <$> files))
 
 -- Given a file name, return (file name and file contents).
 -- Use @readFile@.
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile path =
+  (,) path <$> (readFile path)
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles files =
+  sequence (getFile <$> files)
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@, @lines@, and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
-
+run path =
+  readFile path >>= \chars -> 
+      getFiles (lines chars) >>= printFiles
+    
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
 main =
-  error "todo: Course.FileIO#main"
+  getArgs >>= \args -> 
+    case args of 
+      (arg:.Nil) -> run arg
+      (_) -> putStrLn "Wrong number of args"
 
 ----
 
